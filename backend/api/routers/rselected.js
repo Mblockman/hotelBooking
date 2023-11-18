6ff74f1fb5273ebee97fb53a982ec1f9af2ea004
Mblockman <drmdim@naver.com>
2023-11-18 13:17:07 +0900

    Add files via upload

--- backend/api/routers/rselected.js
+++ backend/api/routers/rselected.js
@@ -3,25 +3,43 @@ const selectedRouter = express.Router();
 const ReservationModel = require('../models/mselected.js');
 
 selectedRouter.get('/list', function(req, res){
+    var url = require('url');
+    var queryData = url.parse(req.url,true).query;
+    if (Object.keys(queryData).length != 0){ req.body = queryData};
     ReservationModel.getReservationList(req, res);
 })
 
 selectedRouter.get('/one', function(req, res){
+    var url = require('url');
+    var queryData = url.parse(req.url,true).query;
+    if (Object.keys(queryData).length != 0){ req.body = queryData};
     ReservationModel.getReservationOne(req, res);
 })
 selectedRouter.get('/typecount', function(req, res){
+    var url = require('url');
+    var queryData = url.parse(req.url,true).query;
+    if (Object.keys(queryData).length != 0){ req.body = queryData};
     ReservationModel.getReservationTypeCount(req, res);
 })
 
 selectedRouter.get('/typelist', function(req, res){
+    var url = require('url');
+    var queryData = url.parse(req.url,true).query;
+    if (Object.keys(queryData).length != 0){ req.body = queryData};
     ReservationModel.getReservationTypeList(req, res);
 })
 
 selectedRouter.get('/rate', function(req, res){
+    var url = require('url');
+    var queryData = url.parse(req.url,true).query;
+    if (Object.keys(queryData).length != 0){ req.body = queryData};
     ReservationModel.getRate(req, res);
 })
 
 selectedRouter.get('/notroomlist', function(req, res){
+    var url = require('url');
+    var queryData = url.parse(req.url,true).query;
+    if (Object.keys(queryData).length != 0){ req.body = queryData};
     ReservationModel.getNotRoomList(req, res);
 })
 
